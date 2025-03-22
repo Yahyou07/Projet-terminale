a190814f249babeb043b020ae968d2ffdb63b540
Yahyou07 <lmassini.yahya@gmail.com>
2025-03-22 20:18:43 +0100

    ajout mouvement joueur
    le joueur est seulement un carré, c'est seulement pour tester

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import pygame,sys
 
+from player import *
 mainClock = pygame.time.Clock()
 from pygame.locals import *
 
 pygame.init()
 pygame.display.set_caption("Test")
+
+
+#Définition de la fenêtre 
+screen = pygame.display.set_mode((500,500),pygame.RESIZABLE)
+
+#Instantiation du player
+player = Player(0,0,screen)
 
 #Fonction quit
 def quit():
@@ -12,14 +20,34 @@ def quit():
             pygame.quit()
             sys.exit()
 
-#Définition de la fenêtre 
-screen = pygame.display.set_mode((500,500),pygame.RESIZABLE)
+#Fonction input pour gerer les entrée clavier
+def input():
+     
+     pressed = pygame.key.get_pressed()
+
+     if pressed[pygame.K_UP] or pressed[pygame.K_z]:
+          player.move_up()
+          
+
+     if pressed[pygame.K_DOWN] or pressed[pygame.K_s] :
+          player.move_down()
+
+     if pressed[pygame.K_RIGHT] or pressed[pygame.K_d]:
+          player.move_right()
+
+     if pressed[pygame.K_LEFT] or pressed[pygame.K_q]:
+          player.move_left()
+    
+
+          
 
 while True : 
     screen.fill((0,0,50))
 
     for event in pygame.event.get():
         quit()
-
+    
+    player.affiche()
+    input()
     pygame.display.update()
     mainClock.tick(60)
--- player.py
+++ player.py
@@ -1,11 +1,25 @@
 
 import pygame
 
-class Player(pygame.sprite.Sprite):
-    def __init__(self,nom):
-        self.nom = nom
+class Player: 
+    def __init__(self,pos_x,pos_y,screen):
+        self.x = pos_x
+        self.y = pos_y
         self.speed = 5
+        self.screen = screen
 
-    
     def affiche(self):
-        pygame.draw()
+        pygame.draw.rect(self.screen,(125,125,125),(self.x,self.y,50,50))
+
+    def move_right(self):
+        self.x +=self.speed
+    
+    def move_left(self):
+        self.x -=self.speed
+    
+    def move_up(self):
+        self.y -=self.speed
+
+    def move_down(self):
+        self.y +=self.speed
+        
