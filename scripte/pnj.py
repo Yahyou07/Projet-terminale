d61c685aeaf70cbc3e3523f5964b99285e5cdc9c
ayyoubAbrk <abarkachayyoub8@gmail.com>
2025-04-25 20:00:51 +0200

    amélioration de la classe pnj
    concernant les animations
    utilisation d'une autre méthode pour l'animation

--- scripte/pnj.py
+++ scripte/pnj.py
@@ -24,7 +24,6 @@ class PNJ(pygame.sprite.Sprite):
         self.image = pygame.transform.scale(self.cropped_image, (int(self.crop_rect.width * self.pourcent), int(self.crop_rect.height * self.pourcent)))
         self.image_color = self.image.get_at((self.image.get_width()-1,self.image.get_height()-1))  # Example: top-left pixel
         self.speed = 1
-
         self.screen = screen
         self.font = pygame.font.Font("Items/Minecraft.ttf", 14)
         self.collide = False
@@ -37,7 +36,91 @@ class PNJ(pygame.sprite.Sprite):
         if self.crop_rect.x >= self.image.get_width() // 13 :
             self.crop_rect.x = 0 # soit self.crop_rect.x -= self.image.get_width() // 13 
     
-        
+    def right(self):
+        """
+            Fonction qui créer une liste d'images pour le mouvement vers la droite.
+        """
+        self.list_right = list()
+
+        for i in range(12):
+            self.crop_rect = pygame.Rect(self.spreadsheet.get_width() // 13 * i, self.spreadsheet.get_height() // 54 * 7, self.spreadsheet.get_width() // 13, self.spreadsheet.get_height() // 54 )
+            self.cropped_image = self.spreadsheet.subsurface(self.crop_rect).copy()
+            self.image = self.cropped_image
+            self.image = pygame.transform.scale(self.image, (int(self.crop_rect.width * self.pourcent), int(self.crop_rect.height * self.pourcent)))
+            self.list_right.append(self.image)
+
+    def left(self):
+        """
+            Fonction qui créer une liste d'images pour le mouvement vers la gauche.
+        """
+        self.list_left = list()
+        for i in range(12):
+            self.crop_rect = pygame.Rect(self.spreadsheet.get_width() // 13 * i, self.spreadsheet.get_height() // 54 * 5, self.spreadsheet.get_width() // 13, self.spreadsheet.get_height() // 54 )
+            self.cropped_image = self.spreadsheet.subsurface(self.crop_rect).copy()
+            self.image = self.cropped_image
+            self.image = pygame.transform.scale(self.image, (int(self.crop_rect.width * self.pourcent), int(self.crop_rect.height * self.pourcent)))
+            self.list_left.append(self.image)
+
+    def up(self):
+        """
+            Fonction qui créer une liste d'images pour le mouvement vers le haut.
+        """
+        self.list_up = list()
+        for i in range(12):
+            self.crop_rect = pygame.Rect(self.spreadsheet.get_width() // 13 * i, self.spreadsheet.get_height() // 54 * 4, self.spreadsheet.get_width() // 13, self.spreadsheet.get_height() // 54 )
+            self.cropped_image = self.spreadsheet.subsurface(self.crop_rect).copy()
+            self.image = self.cropped_image
+            self.image = pygame.transform.scale(self.image, (int(self.crop_rect.width * self.pourcent), int(self.crop_rect.height * self.pourcent)))
+            self.list_up.append(self.image)
+
+    def down(self):
+        """
+            Fonction qui créer une liste d'images pour le mouvement vers le bas.
+        """
+        self.list_down = list()
+        for i in range(12):
+            self.crop_rect = pygame.Rect(self.spreadsheet.get_width() // 13 * i, self.spreadsheet.get_height() // 54 * 6, self.spreadsheet.get_width() // 13, self.spreadsheet.get_height() // 54 )
+            self.cropped_image = self.spreadsheet.subsurface(self.crop_rect).copy()
+            self.image = self.cropped_image
+            self.image = pygame.transform.scale(self.image, (int(self.crop_rect.width * self.pourcent), int(self.crop_rect.height * self.pourcent)))
+            self.list_down.append(self.image)
+
+    def static_right(self):
+        """
+            Fonction qui créer une liste d'images pour le mouvement statique vers la droite.
+        """
+        self.list_static_right = list()
+        for i in range(6):
+            self.crop_rect = pygame.Rect(self.spreadsheet.get_width() // 13 * i, self.spreadsheet.get_height() // 54 * 3, self.spreadsheet.get_width() // 13, self.spreadsheet.get_height() // 54 )
+            self.cropped_image = self.spreadsheet.subsurface(self.crop_rect).copy()
+            self.image = self.cropped_image
+            self.image = pygame.transform.scale(self.image, (int(self.crop_rect.width * self.pourcent), int(self.crop_rect.height * self.pourcent)))
+            self.list_static_right.append(self.image)
+
+    def static_left(self):
+        """
+            Fonction qui créer une liste d'images pour le mouvement statique vers la gauche.
+        """
+        self.list_static_left = list()
+        for i in range(6):
+            self.crop_rect = pygame.Rect(self.spreadsheet.get_width() // 13 * i, self.spreadsheet.get_height() // 54 * 1, self.spreadsheet.get_width() // 13, self.spreadsheet.get_height() // 54 )
+            self.cropped_image = self.spreadsheet.subsurface(self.crop_rect).copy()
+            self.image = self.cropped_image
+            self.image = pygame.transform.scale(self.image, (int(self.crop_rect.width * self.pourcent), int(self.crop_rect.height * self.pourcent)))
+            self.list_static_left.append(self.image)
+
+    def static_down(self):
+        """
+            Fonction qui créer une liste d'images pour le mouvement statique vers le bas.
+        """
+        self.list_static_down = list()
+        for i in range(6):
+            self.crop_rect = pygame.Rect(self.spreadsheet.get_width() // 13 * i, self.spreadsheet.get_height() // 54 * 2, self.spreadsheet.get_width() // 13, self.spreadsheet.get_height() // 54 )
+            self.cropped_image = self.spreadsheet.subsurface(self.crop_rect).copy()
+            self.image = self.cropped_image
+            self.image = pygame.transform.scale(self.image, (int(self.crop_rect.width * self.pourcent), int(self.crop_rect.height * self.pourcent)))
+            self.list_static_down.append(self.image)
+
 
     def static(self):
         """
@@ -136,18 +219,15 @@ class PNJ(pygame.sprite.Sprite):
         self.cropped_image = self.spreadsheet.subsurface(self.crop_rect).copy()
         self.image = self.cropped_image
         self.image = pygame.transform.scale(self.image, (int(self.crop_rect.width * self.pourcent), int(self.crop_rect.height * self.pourcent)))
-        while self.right:
-            time.sleep(0.1)    # Pause de 0.01 seconde pour ralentir le mouvement
-            self.rect.x += 1
-            if self.crop_rect.x < self.spreadsheet.get_width() // 13 :
+        self.rect.x += 1
+        if self.crop_rect.x < self.spreadsheet.get_width() // 13 :
                 self.crop_rect.x += self.spreadsheet.get_width() // 13
-            elif self.crop_rect.x == self.spreadsheet.get_width() // 13 :
+        elif self.crop_rect.x == self.spreadsheet.get_width() // 13 :
                 self.crop_rect.x = 0
-            self.cropped_image = self.spreadsheet.subsurface(self.crop_rect).copy()
-            self.image = self.cropped_image
-            self.image = pygame.transform.scale(self.image, (int(self.crop_rect.width * self.pourcent), int(self.crop_rect.height * self.pourcent)))
-            self.screen.blit(self.image, self.rect)
-            pygame.display.update()
+        self.cropped_image = self.spreadsheet.subsurface(self.crop_rect).copy()
+        self.image = self.cropped_image
+        self.image = pygame.transform.scale(self.image, (int(self.crop_rect.width * self.pourcent), int(self.crop_rect.height * self.pourcent)))
+        self.screen.blit(self.image, self.rect)
             
     def pattern(self,lim_x1,lim_y1,lim_x2,lim_y2):
         """
@@ -171,7 +251,7 @@ class PNJ(pygame.sprite.Sprite):
 
         while not self.collide: # on lui fait faire suivre un patern tant qu'il n'y a pas de collision
             time.sleep(0.01)  # Pause de 0.01 seconde pour ralentir le mouvement
-            if self.rect.x == lim_x1 and self.rect.y == lim_y1:
+            if self.rect.x >= lim_x1 and self.rect.y == lim_y1:
                 self.top = False
                 self.left = False
                 self.bottom = True
