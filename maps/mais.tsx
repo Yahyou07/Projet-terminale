8246dc3098158630e3e1abb00882ae0e9e2c1215
Yahyou07 <lmassini.yahya@gmail.com>
2025-05-15 23:35:17 +0200

    modif

---
+++ maps/mais.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="mais" tilewidth="16" tileheight="16" tilecount="72" columns="9">
+ <image source="TileSet image/fae9739c81a602cfe520b62ac2444f89.png" width="149" height="130"/>
+</tileset>
---
+++ maps/maison.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="maison" tilewidth="32" tileheight="32" tilecount="1" columns="1">
+ <image source="TileSet image/fae9739c81a602cfe520b62ac2444f89.png" width="60" height="60"/>
+</tileset>
