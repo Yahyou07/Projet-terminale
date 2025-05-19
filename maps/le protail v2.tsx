3113a099a79cd90e6cde7621f532ba77d0890a57
Yahyou07 <lmassini.yahya@gmail.com>
2025-05-19 20:12:22 +0200

    modifs de la map de tuto

---
+++ maps/le protail v2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="le protail v2" tilewidth="16" tileheight="16" tilecount="144" columns="12">
+ <image source="TileSet image/portails test.png" width="200" height="200"/>
+</tileset>
